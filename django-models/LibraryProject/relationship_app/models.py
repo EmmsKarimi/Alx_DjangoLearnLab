aa4198586d41407024034a7f6db6b872a02e4959
Emma Karimi <emmaannkinyua@gmail.com>
2025-03-19 16:26:43 +0300

    Added permission_required decorator to secure book management views

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -19,13 +19,13 @@ class Author(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")  # Added related_name
 
     class Meta:
         permissions = [
-            ("can_add_book", "Can add book"),
-            ("can_change_book", "Can edit book"),
-            ("can_delete_book", "Can delete book"),
+            ("can_add_book", "Can add a book"),  # Ensured wording matches views.py
+            ("can_change_book", "Can edit a book"),
+            ("can_delete_book", "Can delete a book"),
         ]
 
     def __str__(self):
@@ -33,28 +33,29 @@ class Book(models.Model):
 
 class Library(models.Model):
     name = models.CharField(max_length=255)
-    books = models.ManyToManyField(Book)
+    books = models.ManyToManyField(Book, related_name="libraries")  # Added related_name
 
     def __str__(self):
         return self.name
 
 class Librarian(models.Model):
     name = models.CharField(max_length=255)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="librarian")  # Added related_name
 
     def __str__(self):
         return self.name
 
 # New Model: UserProfile for Role-Based Access Control
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)  # Link to Django User model
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='Member')  # Default role is 'Member'
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")  # Added related_name
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='Member')
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
 
-# Signal to automatically create a UserProfile when a new User is created
+# Signal to automatically create and save UserProfile when a new User is created
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
+    instance.profile.save()
