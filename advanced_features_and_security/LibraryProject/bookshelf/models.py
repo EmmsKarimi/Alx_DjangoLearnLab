2522ebc47c0002bbff56bba0063b695b416e0ac7
Emma Karimi <emmaannkinyua@gmail.com>
2025-03-21 00:15:08 +0300

    Move bookshelf inside LibraryProject

---
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -0,0 +1,6 @@
+# bookshelf/models.py (must exist even if empty)
+from django.db import models
+
+# Example model (if needed)
+class ExampleModel(models.Model):
+    name = models.CharField(max_length=255)
