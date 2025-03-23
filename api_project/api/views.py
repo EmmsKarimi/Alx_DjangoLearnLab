f62e50ea1005454a254e70addebf49f94f8ccf39
Emma Karimi <emmaannkinyua@gmail.com>
2025-03-23 21:39:45 +0300

    Fixed BookList view to extend ListAPIView

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,7 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView  # Ensure ListAPIView is imported
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(ListAPIView):
+class BookList(ListAPIView):  # Ensure the class extends ListAPIView
     queryset = Book.objects.all()
     serializer_class = BookSerializer
