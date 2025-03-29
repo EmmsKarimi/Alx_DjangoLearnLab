1651087f1f14bb5d177d378d1103605b78c94353
Emma Karimi <emmaannkinyua@gmail.com>
2025-03-29 21:19:22 +0300

    Added profile update view in views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import UserUpdateForm
 
 def home(request):
     return render(request, 'blog/home.html')
+
+@login_required
+def profile(request):
+    if request.method == "POST":
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect("profile")  # Refresh page after saving
+    else:
+        form = UserUpdateForm(instance=request.user)
+    return render(request, "blog/profile.html", {"form": form})
