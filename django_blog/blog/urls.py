d9bf9aa0cd0a66b6dcfcfca3688440837c5303a5
Emma Karimi <emmaannkinyua@gmail.com>
2025-03-29 22:01:43 +0300

    Added login, register, and profile routes in urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import home
+from django.contrib.auth import views as auth_views
+from . import views
+from .views import profile
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', views.home, name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', profile, name='profile'),
 ]
