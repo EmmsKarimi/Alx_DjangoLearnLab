3bce366758a03d02e1ebc71e4a2754ce1b47007c
Emma Karimi <emmaannkinyua@gmail.com>
2025-03-29 22:47:44 +0300

    Added comment functionality to blog posts

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,52 +1,79 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post, Comment
-from .forms import UserUpdateForm, CommentForm
+from .forms import CommentForm
 
-def home(request):
-    return render(request, 'blog/home.html')
+# Blog Post Views
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'  # Template should be blog/home.html
+    context_object_name = 'posts'
+    ordering = ['-created_at']  # Show latest posts first
 
-@login_required
-def profile(request):
-    if request.method == "POST":
-        form = UserUpdateForm(request.POST, instance=request.user)
-        if form.is_valid():
-            form.save()
-            return redirect("profile")  # Refresh page after saving
-    else:
-        form = UserUpdateForm(instance=request.user)
-    return render(request, "blog/profile.html", {"form": form})
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
 
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # Assign logged-in user as author
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  # Allow only the author to update
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = '/'  # Redirect to home after deletion
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  # Allow only the author to delete
+
+# Comment Views
 @login_required
 def add_comment(request, post_id):
     post = get_object_or_404(Post, id=post_id)
-    if request.method == "POST":
+    if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
             comment.author = request.user
             comment.save()
-            return redirect('post_detail', post_id=post.id)
+            return redirect('post_detail', pk=post.id)
     else:
         form = CommentForm()
-    return render(request, 'blog/add_comment.html', {'form': form, 'post': post})
+    return render(request, 'blog/add_comment.html', {'form': form})
 
 @login_required
 def edit_comment(request, comment_id):
     comment = get_object_or_404(Comment, id=comment_id, author=request.user)
-    if request.method == "POST":
+    if request.method == 'POST':
         form = CommentForm(request.POST, instance=comment)
         if form.is_valid():
             form.save()
-            return redirect('post_detail', post_id=comment.post.id)
+            return redirect('post_detail', pk=comment.post.id)
     else:
         form = CommentForm(instance=comment)
-    return render(request, 'blog/edit_comment.html', {'form': form, 'comment': comment})
+    return render(request, 'blog/edit_comment.html', {'form': form})
 
 @login_required
 def delete_comment(request, comment_id):
     comment = get_object_or_404(Comment, id=comment_id, author=request.user)
     post_id = comment.post.id
     comment.delete()
-    return redirect('post_detail', post_id=post_id)
+    return redirect('post_detail', pk=post_id)
