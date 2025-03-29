d24a920e7811cbc7a4c0071df86c401600f425a4
Emma Karimi <emmaannkinyua@gmail.com>
2025-03-29 23:29:56 +0300

    Updated PostForm to include tags with TagWidget

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Comment
+from taggit.forms import TagWidget
+from .models import Comment, Post
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -14,3 +15,13 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+
+class PostForm(forms.ModelForm):
+    tags = forms.CharField(
+        required=False,
+        widget=TagWidget(attrs={'class': 'form-control', 'placeholder': 'Add tags separated by commas'})
+    )
+
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
